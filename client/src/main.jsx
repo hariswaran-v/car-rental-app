0b2c999c132011480dace27c5442d23831e825bb
hariswara <hariswaranvenkatesh@gmail.com>
2025-08-20 07:47:03 +0530

    feat: footer ui update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // <-- change here
 import { AppProvider } from "./context/AppContext.jsx";
 import { MotionConfig } from "framer-motion";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppProvider>
       <MotionConfig viewport={{ once: true }}>
         <App />
       </MotionConfig>
     </AppProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
